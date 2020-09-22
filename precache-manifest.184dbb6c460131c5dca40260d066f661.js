2a40b553d68b12455bf4c0c01bb620ae6f1f4a3c
Mercy Bhakta <mercybhakta@yahoo.com>
2020-09-22 14:39:34 -0700

    Updates

---
+++ precache-manifest.184dbb6c460131c5dca40260d066f661.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a0146ba6c382954282b7e593be336c8",
+    "url": "/Todo-React-App/index.html"
+  },
+  {
+    "revision": "4771228e9f0c14ba58b0",
+    "url": "/Todo-React-App/static/css/main.15a744f8.chunk.css"
+  },
+  {
+    "revision": "b0bc859f895acf212bf5",
+    "url": "/Todo-React-App/static/js/2.0382a6f3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Todo-React-App/static/js/2.0382a6f3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4771228e9f0c14ba58b0",
+    "url": "/Todo-React-App/static/js/main.a33d4932.chunk.js"
+  },
+  {
+    "revision": "5be4bc9f2e3f2b5f0e40",
+    "url": "/Todo-React-App/static/js/runtime-main.270b3318.js"
+  }
+]);
