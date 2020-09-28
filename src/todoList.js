ffd13464c19254d18bdc7d003407f08c441cfd6c
Mercy Bhakta <mercybhakta@yahoo.com>
2020-09-28 12:56:38 -0700

    Pushing source code + github ribbon

---
+++ src/todoList.js
@@ -0,0 +1,152 @@
+import React, { Component } from "react";
+import './todoList.css';
+
+class Items extends Component {
+    constructor(props) {
+        super(props);
+        this.createTasks = this.createTasks.bind(this);
+    }
+
+    delete(key) {
+        this.props.delete(key);
+    }
+
+    createTasks(item) {
+        return ( <li onClick={(e) => e.target.classList.toggle("crossedOut")}
+                     key={item.key}>{item.text}
+                <button className="delete"
+                        onClick={() => this.delete(item.key)}>×
+                </button>
+                </li>
+
+               )
+    }
+
+    render() {
+        let todoEntries = this.props.entries;
+        let listItems = todoEntries.map(this.createTasks);
+
+        let listStyle = {
+            padding: 0
+        };
+        return (
+            <ul id="source-html" style={listStyle} className="theList">
+                {listItems}
+            </ul>
+        );
+    }
+}
+
+class List extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { items: [],
+                       title: 'To Do: '};
+
+        this.addItem = this.addItem.bind(this);
+        this.deleteItem = this.deleteItem.bind(this);
+        this.updateTitle = this.updateTitle.bind(this);
+        this.exportHTML = this.exportHTML.bind(this);
+    }
+
+
+    addItem(e) {
+        if (this._inputElement.value !== "") {
+            let newItem = {
+                text: this._inputElement.value,
+                key: Date.now()
+            };
+
+            this.setState((prevState) => {
+                return {
+                    items: prevState.items.concat(newItem)
+                };
+            });
+            this._inputElement.value = "";
+        }
+        console.log(this.state.items);
+        e.preventDefault();
+    }
+
+    deleteItem(key) {
+        let filteredItems = this.state.items.filter(function (item) {
+            return (item.key !== key);
+        });
+        this.setState({
+            items: filteredItems
+        });
+    }
+
+    updateTitle(e) {
+        if (this._titleElement.value !== "") {
+            //this.state.title = this._titleElement.value;
+            this.setState({title: this._titleElement.value});
+            let title = document.querySelector(".title");
+            title.placeholder = this.state.title;
+            this._titleElement.value = "";
+        }
+        e.preventDefault();
+    }
+
+    exportHTML() {
+        let header = "<html xmlns:o='urn:schemas-microsoft-com:office:office' "+
+            "xmlns:w='urn:schemas-microsoft-com:office:word' "+
+            "xmlns='http://www.w3.org/TR/REC-html40'>"+
+            "<head><meta charset='utf-8'><title>Export HTML to Word Document with JavaScript</title></head><body>";
+
+        let footer = "</body></html>";
+        let title = this.state.title;
+        let sourceHTML = header+title+document.getElementById("source-html").innerHTML+footer;
+        //get rid of delete × from word doc
+        sourceHTML = sourceHTML.replace(/×/g, '');
+        let source = 'data:application/vnd.ms-word;charset=utf-8,' + encodeURIComponent(sourceHTML);
+        let fileDownload = document.createElement("a");
+        document.body.appendChild(fileDownload);
+        fileDownload.href = source;
+        fileDownload.download = title + '.doc';
+        fileDownload.click();
+        document.body.removeChild(fileDownload);
+        console.log("exported successfully!")
+    }
+
+    render() {
+        
+        let inputStyle = {
+            padding: 10,
+            fontSize: 16,
+            borderWidth: 2,
+            borderStyle: "solid",
+            width: 165
+        };
+
+
+        return(
+            <div>
+                <div>
+                    <form onSubmit={this.updateTitle}>
+                        <input ref={(a) => this._titleElement = a}
+                               className="title" type="text"
+                               placeholder={this.state.title}>
+                        </input>
+                    </form>
+
+                    <form onSubmit={this.addItem}>
+                        <input style={inputStyle}
+                               ref={(a) => this._inputElement = a}
+                               placeholder="enter task">
+                        </input>
+                        <button type="submit" className="uiButton">add</button>
+                    </form>
+                </div>
+                <Items entries={this.state.items}
+                           delete={this.deleteItem}/>
+               <div>
+                   <button id="btn-export" className="uiButton" onClick={this.exportHTML}> Export to Word</button>
+               </div>
+            </div>
+        );
+    }
+}
+
+
+export default List;
